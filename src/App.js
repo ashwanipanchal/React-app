fc824971369cfd8d6353516dbc78488928d6bd80
ashwani panchal <ashwani.panchal@hotmail.com>
2020-01-28 17:55:57 +0530

    added API call

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Title from './components/Title';
 import Form from './components/Form';
@@ -11,7 +10,9 @@ class App extends React.Component {
 
   getWeather = async(e) => {
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=delhi,in&appid=${API_KEY}`);
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
   }
@@ -20,7 +21,7 @@ class App extends React.Component {
     return (
       <div>
         <Title/>
-        <Form/>
+        <Form getWeather={this.getWeather}/>
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Form extends React.Component {
   render() {
     return (
-        <form>
+        <form onSubmit={this.props.getWeather}>
             <input type="text" name="city" placeholder="City.."/><br/>
             <input type="text" name="country" placeholder="Country.."/><br/>
             <button>Get Weather</button>
